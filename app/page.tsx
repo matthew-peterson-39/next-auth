ea6425747f2b478a83a2b0c93e265e67294075c8
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-19 07:52:44 -0500

    gradient added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
   <>
-    <Button size="lg" variant="link">
-      Click Me
-    </Button>
-  </>
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-slate-800">
+
+    </main>
+  </> 
   );
 }
