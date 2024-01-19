1f25873d65d6ea70dde33785775e44d02562f8db
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-19 13:37:52 -0500

    Add register functionality

--- actions/login.ts
+++ actions/login.ts
@@ -5,8 +5,10 @@ import { LoginSchema } from "@/schemas";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const vlaidatedFields = LoginSchema.safeParse(values);
+    
     if (!vlaidatedFields.success) { 
         return {error: "Invalid field(s)!"};
     }
+
     return {success: "Email sent!"};
 }
