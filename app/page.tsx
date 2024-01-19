04b4e7d2340e8486e1e4364232ab2f744e8dd29d
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-19 08:12:45 -0500

    add login-button util

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const FONT = Poppins({subsets: ["latin"], weight:["600"]})
 
 export default function Home() {
   return (
   <>
     <main className="flex h-full flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-slate-800">
-
+      <div className="flex flex-col items-center justify-center space-y-6 text-center">
+        <h1 className={cn("text-6xl text-center font-semibold text-white drop-shadow-md", FONT.className)}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+        <div className="flex">       
+        {/* <LoginButton mode="modal"> */}
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
     </main>
   </> 
   );
