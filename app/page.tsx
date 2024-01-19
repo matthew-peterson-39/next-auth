b5d1ec6724f6f84016cf6072d650ddb3749ca5b7
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-19 00:28:21 -0500

    removed p element

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
   <>
-    <p className="font-semibold">hello world</p>
     <Button size="lg" variant="link">
       Click Me
     </Button>
