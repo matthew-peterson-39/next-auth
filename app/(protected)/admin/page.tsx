1debd99f802ad617153383aaaf2e702e3c1aa2e2
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-20 09:41:27 -0500

    add RoleGate for role restriction

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { 
+    Card, 
+    CardContent, 
+    CardHeader 
+} from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
+
+export const AdminPage = () => {
+    return (
+        <Card className="w-[600px]">
+            <CardHeader >
+                <p className="text-2xl font-semibold text-center">
+                    Admin
+                </p>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <RoleGate allowedRole={UserRole.ADMIN}>
+                    <FormSuccess message="You are allowed to see this content."/>
+                </RoleGate>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default AdminPage;
