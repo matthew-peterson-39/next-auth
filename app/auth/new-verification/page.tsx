3a58b36c50e187c8b2ee419061dd839b04dc0997
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-19 20:04:38 -0500

    added email verification functionality

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,13 @@
+import NewVerificationForm from "@/components/auth/new-verification-form"
+
+const NewVerificationPage = () => {
+  return (
+  <>
+    <NewVerificationForm>
+
+      </NewVerificationForm>
+  </>
+  )
+}
+
+export default NewVerificationPage
